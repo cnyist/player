0d12606f94881ea755a5603c6ba696c5d072f0a7
yunwanjia-cloud <371622558@qq.com>
2020-12-19 23:09:40 +0800

    bug

--- js/generate.js
+++ js/generate.js
@@ -7,10 +7,10 @@ function generate(music_id){
     music_time = document.getElementById("player").contentWindow.musicList[index]
     artist = document.getElementById("player").contentWindow.musicList[index]['artist']
 
-    innerHTML = "<li class=\"songlist__item\"><div class=\"songlist__item_box\" style=\"visibility: visible;\"><div class=\"songlist__link mod_cover\" onclick=\"$(\'#player\').contents().find(\'#" + 
-    music_id + "\')[0].click(); $(\'#player\').contents().find(\'#btnfold\')[0].click();\"><img class=\"songlist__pic\" src=\"" + 
-    poster + "\" style=\"display: block; visibility: visible;\"><i class=\"mod_cover__mask\"></i><i class=\"mod_cover__icon_play js_play\"></i></div><div class=\"songlist__cont\"><h3 class=\"songlist__song\"><div class=\"js_song\" title=\"" + 
-    title + "\">" + title + "</div></h3><p class=\"songlist__author\" title=\"" + 
+    innerHTML = "<li class=\"song_item\"><div class=\"song_item_box\" style=\"visibility: visible;\"><div class=\"song_link mod_cover\" onclick=\"$(\'#player\').contents().find(\'#" + 
+    music_id + "\')[0].click(); $(\'#player\').contents().find(\'#btnfold\')[0].click();\"><img class=\"song_pic\" src=\"" + 
+    poster + "\" style=\"display: block; visibility: visible;\"><i class=\"mod_cover_mask\"></i><i class=\"mod_cover_icon_play js_play\"></i></div><div class=\"song_cont\"><h3 class=\"song_song\"><div class=\"js_song\" title=\"" + 
+    title + "\">" + title + "</div></h3><p class=\"song_author\" title=\"" + 
     artist + "\"></p><div title=\"" + artist + "\" class=\"c_tx_thin singer_name\">" + artist + "</div></div></div></li>";
 
     music.innerHTML = innerHTML;
